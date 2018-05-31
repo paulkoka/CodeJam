cd11c8fc9fe652b13e3b967415f71cc1871393f9
paulkoka <pavelkoka@gmail.com>
2018-05-31 18:32:36 +0300

    Initial Commit

---
+++ CodeJam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeJam
+//
+//  Created by paul on 5/31/18.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeJam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeJam
+//
+//  Created by paul on 5/31/18.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
